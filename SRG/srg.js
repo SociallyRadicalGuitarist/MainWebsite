37026215672c208ef491de451a306b5faf5b9cfc
CShingiro <cshingiro7202@conestogac.on.ca>
2021-08-08 23:02:35 -0400

    Website Upload

---
+++ SRG/srg.js
@@ -0,0 +1,27 @@
+"use strict"
+
+(() => { 
+    var firebaseConfig = {
+
+        apiKey: "AIzaSyCLsQJzdyai3359GDS-m8ASQ5aehUxyULQ",
+
+        authDomain: "socially-radical-guitarist.firebaseapp.com",
+
+        projectId: "socially-radical-guitarist",
+
+        storageBucket: "socially-radical-guitarist.appspot.com",
+
+        messagingSenderId: "757690216779",
+
+        appId: "1:757690216779:web:79b59078b3415c295d78ce",
+
+        measurementId: "G-0FZ0X6KH23"
+
+    };
+
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+  firebase.analytics();
+})
